a5be5df3c83b3cc2d1a3a50f7064bf1c3779578a
Serlin Tamilselvam <t.serlin94@gmail.com>
2021-05-30 17:40:23 -0400

    FORM validation done, axios request done

---
+++ src/api.js
@@ -0,0 +1,7 @@
+export const APISettings = {
+    backEndUrl: process.env.VUE_APP_BACKEND_URL,
+    adminEmailAddress: process.env.VUE_APP_ADMIN_EMAIL_ADDRESS,
+    email: {
+        sendToAdmin: 'email/send/info'
+    }
+}
--- src/components/Sections/Contact/Contact.js
+++ src/components/Sections/Contact/Contact.js
@@ -1,3 +1,14 @@
+import Vue from 'vue'
+import Vuelidate from 'vuelidate'
+import { minLength, required, email } from "vuelidate/lib/validators"
+
+import EmailService from '../../../services/EmailService'
+import { APISettings } from '../../../api'
+
+const EmailObj = new EmailService()
+
+Vue.use(Vuelidate)
+
 export default {
     name: 'Contact',
     props: {
@@ -6,10 +17,27 @@ export default {
         return {
             pageName: "contact",
             isDivisionShown: false,
+            submitStatus: null,
             formData: {
                 name: '',
                 email: '',
                 message: ''
+            }
+        }
+    },
+    validations: {
+        formData: {
+            name: {
+                required,
+                minLength: minLength(4)
+            },
+            email: {
+                required,
+                email
+            },
+            message: {
+                required,
+                minLength: minLength(5)
             }
         }
     },
@@ -18,7 +46,49 @@ export default {
             this.isDivisionShown = isVisible
         },
         onSubmit() {
-            console.log("On Submit")
+            this.$v.$touch()
+            if (this.$v.$invalid) {
+              this.submitStatus = 'ERROR'
+            } else {
+              this.submitStatus = 'PENDING'
+            }
+            this.triggerEmail()
+        },
+        constructFormData() {
+            return {
+                "to": APISettings.adminEmailAddress,
+                "subject": "Email from portfolio website",
+                "text" : "Name: " + this.formData.name + "\nEmail: " + this.formData.email 
+                            + "\nMessage: " + this.formData.message
+            }
+        },
+        clearFormData() {
+            this.formData.name = ""
+            this.formData.message = ""
+            this.formData.email = ""
+        },
+        triggerEmail() {
+            //Perform axios call to send email
+            let data = this.constructFormData()
+
+            EmailObj.sendAdmin(data).then((response) => {
+                let responseData = response.data.response
+                if(responseData.result === 'success') {
+                    // Hide Loader
+                    alert("Thnk you for sending email, I will respond to it quickly")
+                    this.submitStatus = 'OK'
+                    this.clearFormData()
+                } else {
+                    // Hide Loader
+                    alert("An error occured while sending email, Please try again")
+                    this.submitStatus = 'OK'
+                }
+                }).catch((err) => {
+                    // Hide Loader
+                    alert("An error occured while sending email, Please try again")
+                    this.submitStatus = 'OK'
+                    console.log(err)
+                })
         }
     }
 }
